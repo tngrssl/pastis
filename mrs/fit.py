- some last fixes regarding ratio CRB calculation - worked a lot on the notebooks

mrs/fit.py
@@ -267,8 +267,10 @@ class prefit_tool:
         self._peak_areas_norm = []
         pars = sim.params(self.meta_bs)
         pars[:] = 0.0
+        pars._linklock[:] = 1
         pars_pnorm = sim.params(self.meta_bs)
         pars_pnorm[:] = 0.0
+        pars_pnorm._linklock[:] = 1
 
         log.info("integrating peak for...")
         log.info_line________________________()
@@ -301,8 +303,11 @@ class prefit_tool:
             # store
             self._peak_areas.append(this_peak_area)
             self._peak_areas_norm.append(this_peak_area / this_peak_np)
-            pars[this_peak_index, 0] = this_peak_area
-            pars_pnorm[this_peak_index, 0] = this_peak_area / this_peak_np
+            pars[this_peak_index, xxx.p_cm] = this_peak_area
+            pars_pnorm[this_peak_index, xxx.p_cm] = this_peak_area / this_peak_np
+            # fix linklock vectors
+            pars._linklock[this_peak_index, xxx.p_cm] = 0
+            pars_pnorm._linklock[this_peak_index, xxx.p_cm] = 0
 
             # display the area
             if(self.display_enable):
@@ -1168,7 +1173,7 @@ def disp_fit(ax, data, params, seq, LL_exluding=True, LL_merging=False, mIndex_l
         # plot the a single MM keeping the same offset
         ax.plot(s_single_MM.frequency_axis_ppm(), s_single_MM.spectrum().real - ygap * (koffset + 2), 'r--')
 
-    ax.text(display_range[1] - 0.5, -ygap * (koffset + 2) + ygap / 5, 'MM baseline')
+    ax.text(display_range[1] - 0.5, -ygap * (koffset + 2) + ygap / 5, 'Lip baseline')
 
     # finalize the plot
     ax.set_xticks(np.arange(-1, 10, 0.5))

pipeline_fit_brain_spinalcord.py
@@ -28,13 +28,16 @@ log.setLevel(log.DEBUG)
 
 rdb = db.data_db("/home/tangir/crmbm/acq_db/brain.pkl")
 
+# fast test: only last fit strategy only
+only_one_fit_strategy = False
+
 # display real-time fit and other stuff?
 display_stuff = False
 
 # %% select datasets via dataframe
 
 df_sel = rdb.df_reco
-# df_sel = rdb.df_reco.loc[(rdb.df_reco["patient"] == 308) & (rdb.df_reco["study"] == 1)]
+# df_sel = rdb.df_reco.loc[(rdb.df_reco["patient"] == 319) & (rdb.df_reco["study"] == 1)]
 # df_sel = rdb.df_reco.loc[(rdb.df_reco.index == "3695a02a4fbf0153215cf09eaa21658e")]
 
 # %% fit strategies
@@ -43,8 +46,8 @@ df_sel = rdb.df_reco
 fit_stategies_list = []
 
 # list of sequence to try
-# fit_strategies_seq_list = [sim.mrs_sequence, sim.mrs_seq_press]
-fit_strategies_seq_list = [sim.mrs_seq_press]
+fit_strategies_seq_list = [sim.mrs_sequence, sim.mrs_seq_press]
+#fit_strategies_seq_list = [sim.mrs_seq_press]
 
 # init metabolite basis set and linklock
 meta_bs = sim.metabolite_basis_set()
@@ -275,7 +278,10 @@ for s in fit_strategies_seq_list:
 
 water_concentration = 55000.0  # mmol/kg
 
-# %% run the fits!
+if(only_one_fit_strategy):
+    fit_stategies_list = [fit_stategies_list[-1]]
+
+# %% prepare data to fit
 
 # browse though datasets
 for this_hash, this_dataset in zip(df_sel.index, df_sel["dataset"]):
@@ -291,6 +297,12 @@ for this_hash, this_dataset in zip(df_sel.index, df_sel["dataset"]):
 
     # removing water and any artefact > 5ppm (corrupts fit quality criteria)
     this_data = this_data.correct_water_removal_1d(8, [4.5, 6], display_stuff)
+
+    # filtering
+    #this_data = this_data.correct_bandpass_filtering_1d([0, 6], np.ones, display_stuff)
+
+    # reapodize to remove filtering artefact (will not affect linewidth because already apodized)
+    #this_data = this_data.correct_apodization_nd(5.0, display_stuff)
 
     # %% prepare simulation machine
 
@@ -388,7 +400,7 @@ for this_hash, this_dataset in zip(df_sel.index, df_sel["dataset"]):
         # ranges for concentration
         fittool.params_min[:, xxx.p_cm] = 0.09
         fittool.params_max[:, xxx.p_cm] = 300.0
-        fittool.params_max[xxx.m_All_MMs, xxx.p_cm] = 600.0
+        fittool.params_max[xxx.m_All_MMs, xxx.p_cm] = 5000.0
         fittool.params_max[xxx.m_Water, xxx.p_cm] = 100000.0
 
         # linewidth bounds for metabolites
@@ -409,6 +421,9 @@ for this_hash, this_dataset in zip(df_sel.index, df_sel["dataset"]):
         fittool.params_init[:, xxx.p_df] = -15.0
         fittool.params_min[:, xxx.p_df] = -35.0
         fittool.params_max[:, xxx.p_df] = 5.0
+        # a bit more for the lipids
+        fittool.params_min[:, xxx.p_df] = -55.0
+        fittool.params_max[:, xxx.p_df] = 25.0
 
         # phase bounds for metabolites and lipids
         fittool.params_min[:, xxx.p_dp] = -0.1
@@ -435,4 +450,8 @@ for this_hash, this_dataset in zip(df_sel.index, df_sel["dataset"]):
                         "params_fit_final": params_fit_final,
                         "optim_result": optim_result}
 
-        rdb.save_fit_results(this_hash, fit_results, fittool, this_fit_strategy)
+        # save in memory only
+        rdb.save_fit_results(this_hash, fit_results, fittool, this_fit_strategy, False)
+
+# now write to pkl file
+rdb.write_pickle_files()