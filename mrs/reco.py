- some last fixes regarding ratio CRB calculation - worked a lot on the notebooks

mrs/reco.py
@@ -1895,7 +1895,7 @@ class MRSData2(suspect.mrsobjects.MRSData):
             axs[1, 1].set_title("Abs. phase = %.2f ± %.2f rad" % (peak_prop_disp[:, 3].mean(), peak_prop_disp[:, 3].std()))
 
             # nice plot showing all raw data
-            plt.subplot(1, 3, 3)
+            ax = plt.subplot(1, 3, 3)
             ppm = s_ma.frequency_axis_ppm()
             ystep = np.max(np.mean(s_ma.spectrum().real, axis=0))
             ystep = np.power(10, 1 + (np.floor(np.log10(ystep))))
@@ -1914,7 +1914,23 @@ class MRSData2(suspect.mrsobjects.MRSData):
             plt.xlim(peak_range[1], peak_range[0])
             plt.xlabel('chemical shift (ppm)')
             plt.ylabel('individual spectra')
-            plt.yticks([])
+
+            # y ticks: spectrum index
+            # TODO: maybe need to calculate this automatically
+            n_yticks = 16
+            step_yticks = 2 ** np.ceil(np.sqrt(s.shape[0] / n_yticks))
+            yt_lbl_list = np.arange(0, s.shape[0], step_yticks).tolist()
+            if(yt_lbl_list[-1] != s.shape[0]):
+                yt_lbl_list = yt_lbl_list + [s.shape[0]]
+            yt_lbl_list = [("%d" % yt) for yt in yt_lbl_list]
+
+            yt_loc_list = np.arange(0, s.shape[0] * ystep, step_yticks * ystep).tolist()
+            if(yt_loc_list[-1] != (s.shape[0] * ystep)):
+                yt_loc_list = yt_loc_list + [s.shape[0] * ystep]
+
+            ax.set_yticks(yt_loc_list)
+            ax.set_yticklabels(labels=yt_lbl_list)
+
             plt.grid('on')
             fig.subplots_adjust()
             fig.show()
@@ -2957,7 +2973,7 @@ class MRSData2(suspect.mrsobjects.MRSData):
         plt.subplots_adjust()
         plt.show()
 
-        return(plt)
+        return(plt.figure(ifig))
 
     def save_ismrmd(self, h5_filepath):
         """

mrs/sim.py
@@ -86,7 +86,7 @@ class params(np.ndarray):
         # the error vector
         self._errors = np.zeros(self.shape)
         # the corr vector
-        self._corr_mat = np.zeros([self.size, self.size])
+        self._corr_mat = None
 
         # freeze
         self.__isfrozen = True
@@ -432,49 +432,51 @@ class params(np.ndarray):
         p2[:, xxx.p_cm] = p1[:, xxx.p_cm] / p1[mIndex, xxx.p_cm]
 
         # deal with errors too
-        # see paper http://dx.doi.org/10.1007/s10334-005-0018-7
-        # relCRB(1/2) = sqrt(relCRB1^2 + relCRB2^2 - 2*corr1_2*relCRB1*relCRB2)
-        # get numerator rel CRBs for cm
-        relCRBs_num = p1.get_errors_prct()[:, xxx.p_cm]
-        # get denominator rel CRBs for cm
-        relCRBs_den = p1.get_errors_prct()[mIndex, xxx.p_cm]
+        # if correlation matrix available
+        if(self.corr_mat is not None):
+            # see paper http://dx.doi.org/10.1007/s10334-005-0018-7
+            # relCRB(1/2) = sqrt(relCRB1^2 + relCRB2^2 - 2*corr1_2*relCRB1*relCRB2)
+            # get numerator rel CRBs for cm
+            relCRBs_num = p1.get_errors_prct()[:, xxx.p_cm]
+            # get denominator rel CRBs for cm
+            relCRBs_den = p1.get_errors_prct()[mIndex, xxx.p_cm]
 
-        # get corr coeff between num and den cm
-        # first, get free param corr mat
-        free_param_corr_mat = self.corr_mat
-        # convert mIndex to free params index
-        p3 = self.copy()
-        # replace cm by index
-        p3[:, xxx.p_cm] = np.arange(0, p3.shape[0], 1)
-        # lock all other pars
-        p3.linklock[:, xxx.p_dd] = 1
-        p3.linklock[:, xxx.p_df] = 1
-        p3.linklock[:, xxx.p_dp] = 1
-        # convert ot free params
-        p3_free = p3.toFreeParams()
-        # here we should get a list of indexes
-        # find where is mIndex and we did it
-        ind_free_pars_mIndex = np.where(p3_free == mIndex)[0][0]
-        # extract corr vector
-        free_param_corr_vec = free_param_corr_mat[:, ind_free_pars_mIndex]
-        # convert it to full params
-        p3 = self.copy()
-        # lock all other pars
-        p3.linklock[:, xxx.p_dd] = 1
-        p3.linklock[:, xxx.p_df] = 1
-        p3.linklock[:, xxx.p_dp] = 1
-        # replace cm by cor coeffs
-        p3 = p3.toFullParams(free_param_corr_vec)
-        # get full param corr vector
-        full_param_corr_vec = p3[:, xxx.p_cm]
-        # make it absolute
-        rc_num_den_abs = np.abs(full_param_corr_vec)
+            # get corr coeff between num and den cm
+            # first, get free param corr mat
+            free_param_corr_mat = self.corr_mat
+            # convert mIndex to free params index
+            p3 = self.copy()
+            # replace cm by index
+            p3[:, xxx.p_cm] = np.arange(0, p3.shape[0], 1)
+            # lock all other pars
+            p3.linklock[:, xxx.p_dd] = 1
+            p3.linklock[:, xxx.p_df] = 1
+            p3.linklock[:, xxx.p_dp] = 1
+            # convert ot free params
+            p3_free = p3.toFreeParams()
+            # here we should get a list of indexes
+            # find where is mIndex and we did it
+            ind_free_pars_mIndex = np.where(p3_free == mIndex)[0][0]
+            # extract corr vector
+            free_param_corr_vec = free_param_corr_mat[:, ind_free_pars_mIndex]
+            # convert it to full params
+            p3 = self.copy()
+            # lock all other pars
+            p3.linklock[:, xxx.p_dd] = 1
+            p3.linklock[:, xxx.p_df] = 1
+            p3.linklock[:, xxx.p_dp] = 1
+            # replace cm by cor coeffs
+            p3 = p3.toFullParams(free_param_corr_vec)
+            # get full param corr vector
+            full_param_corr_vec = p3[:, xxx.p_cm]
+            # make it absolute
+            rc_num_den_abs = np.abs(full_param_corr_vec)
 
-        # calculate the final relCRB
-        rel_CRBs_ratio = np.sqrt( relCRBs_num**2 + relCRBs_den**2 - 2 * rc_num_den_abs * relCRBs_num * relCRBs_den )
-        # back to absCRB
-        abs_CRBs_ratio = p2[:, xxx.p_cm] * rel_CRBs_ratio / 100.0
-        p2._errors[:, xxx.p_cm] = abs_CRBs_ratio
+            # calculate the final relCRB
+            rel_CRBs_ratio = np.sqrt( relCRBs_num**2 + relCRBs_den**2 - 2 * rc_num_den_abs * relCRBs_num * relCRBs_den )
+            # back to absCRB
+            abs_CRBs_ratio = p2[:, xxx.p_cm] * rel_CRBs_ratio / 100.0
+            p2._errors[:, xxx.p_cm] = abs_CRBs_ratio
 
         return(p2)
 