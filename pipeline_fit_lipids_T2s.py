e6a15006117256cac763f831a97d5192fadb83e0
tangir <tangi.roussel@univ-amu.fr>
2021-04-29 18:03:11 +0200

    * Fitting lipid spectrum :)

--- pipeline_fit_lipids_T2s.py
+++ pipeline_fit_lipids_T2s.py
@@ -15,6 +15,7 @@ import mrs.log as log
 
 import pandas as pd
 import numpy as np
+import os
 
 get_ipython().magic("clear")
 plt.close("all")
@@ -39,19 +40,21 @@ df = df.loc[df["reco_dataset_legend"].str.contains("T2 estimation 23/04/2021")]
 meta_bs = sim.metabolite_basis_set("Lipids")
 
 metabolites_list = np.sort([
-    xxx.m_Water])
+                    xxx.m_Water,
+                    xxx.m_LipA,
+                    xxx.m_LipB,
+                    xxx.m_LipC,
+                    xxx.m_LipD,
+                    xxx.m_LipE,
+                    xxx.m_LipF,
+                    xxx.m_LipG,
+                    xxx.m_LipH,
+                    xxx.m_LipI,
+                    xxx.m_LipJ])
 
-[ xxx.m_LipA,
- xxx.m_LipB,
- xxx.m_LipC,
- xxx.m_LipD,
- xxx.m_LipE,
- xxx.m_LipF,
- xxx.m_LipG,
- xxx.m_LipH,
- xxx.m_LipI,
- xxx.m_LipJ]
 # %% fit each dataset
+
+fit_results_list = []
 
 for this_data in df["reco_dataset_dcm_data_obj"].to_list():
 
@@ -61,7 +64,7 @@ for this_data in df["reco_dataset_dcm_data_obj"].to_list():
     fittool.metabolites = metabolites_list
     fittool.metabolites_area_integration = metabolites_list
     fittool.display_range_ppm = [0, 6]
-    fittool.display_CRBs = False
+    # fittool.display_CRBs = False
 
     # default fitting bounds from muscle template
     fittool.params_min = fittool.params_min.set_default_min()
@@ -70,20 +73,24 @@ for this_data in df["reco_dataset_dcm_data_obj"].to_list():
     # ranges for concentration
     fittool.params_min[:, xxx.p_cm] = -1
     fittool.params_min[metabolites_list, xxx.p_cm] = 0
-    fittool.params_max[:, xxx.p_cm] = 1000.0
+    fittool.params_max[:, xxx.p_cm] = 5000.0
     # initial concentrations
     fittool.params_init[:, xxx.p_cm] = 0
     fittool.params_init[metabolites_list, xxx.p_cm] = 0.1
 
     # linewidth bounds for metabolites
-    fittool.params_min[:, xxx.p_dd] = 1.0
+    fittool.params_min[:, xxx.p_dd] = 10.0
     fittool.params_max[:, xxx.p_dd] = 300.0
     # initial damping
     fittool.params_init[:, xxx.p_dd] = 100
 
     # frequency shifts
-    fittool.params_min[:, xxx.p_df] = -20.0
-    fittool.params_max[:, xxx.p_df] = 20.0
+    fittool.params_min[:, xxx.p_df] = -5.0
+    fittool.params_max[:, xxx.p_df] = 5.0
+    # water
+    fittool.params_min[xxx.m_Water, xxx.p_df] = 10.0
+    fittool.params_max[xxx.m_Water, xxx.p_df] = 20.0
+    fittool.params_init[xxx.m_Water, xxx.p_df] = +13
 
     # phase shifts
     fittool.params_min[:, xxx.p_dp] = -0.1
@@ -91,10 +98,21 @@ for this_data in df["reco_dataset_dcm_data_obj"].to_list():
 
     # linklock: global phase, frequency and damping, independant amplitudes
     fittool.params_linklock[:] = 1
-    fittool.params_linklock[metabolites_list, :] = [0, 200, 300, 100]
-    fittool.params_linklock[xxx.m_LipA, :] = [0, -200, -300, -100]
+    fittool.params_linklock[metabolites_list, :] = [0, 200, 0, 100]
+    fittool.params_linklock[xxx.m_LipA, :] = [0, -200, 0, -100]
+
     # leave water free
     fittool.params_linklock[xxx.m_Water, :] = [0, 0, 0, 0]
 
     # run the fit
     fittool.run()
+
+    # --- save the fit results ---
+    fittool_df = fittool.to_dataframe('lip_')
+    fittool_df = fittool_df.set_index(["lip_fit_hash", "lip_data_file_hash"])
+    fit_results_list.append(fittool_df)
+
+# append all the results and store
+df_fit_results = pd.concat(fit_results_list, axis=0)
+db_filepath_noext, _ = os.path.splitext(db_filepath)
+df_fit_results.to_pickle(db_filepath_noext + "_fit.pkl")
