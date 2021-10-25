07f1ec165534a44e3aee46151a3d1b6f30bdb32a
tngrssl <tangi.roussel@univ-amu.fr>
2021-10-25 18:46:15 +0200

    Added setup.py (forgot it) !

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pastis',
+version='0.0.1',
+description="",
+author="Tangi Roussel",
+packages=["mrs"],
+install_requires=['pygamma','numpy', 'suspect', 'pandas','pymapvbvd', 'xlrd', 'termcolor']
+)
