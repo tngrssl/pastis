20cb1eface17235b1ae27d1ca7457a94d2fbe2de
tangir <tangi.roussel@univ-amu.fr>
2022-01-12 11:15:18 +0100

    Fixed setup file

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ version='0.0.1',
 description="",
 author="Tangi Roussel",
 packages=["mrs"],
-install_requires=['pygamma','numpy', 'suspect', 'pandas', 'pymapvbvd', 'xlrd', 'termcolor', 'nibabel', "json"]
+install_requires=['pygamma','numpy', 'suspect', 'pandas', 'pymapvbvd', 'xlrd', 'termcolor', 'nibabel', 'IPython', 'ismrmrd']
 )
