fa345ea210a30d58ea86074508c1e0d0d0852b33
tngrssl <tangi.roussel@univ-amu.fr>
2022-01-12 11:58:32 +0100

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='pastis',
-version='0.0.1',
+version='0.1.0',
 description="",
 author="Tangi Roussel",
-packages=["mrs"],
+packages=["pastis"],
 install_requires=['pygamma','numpy', 'suspect', 'pandas', 'pymapvbvd', 'xlrd', 'termcolor', 'nibabel', 'IPython', 'ismrmrd']
 )
