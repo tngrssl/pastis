77bdeb6bf2781f19f9c86446308c32ace867ea48
tangir <tangi.roussel@univ-amu.fr>
2022-04-06 15:51:37 +0200

    Removed ismrmd from setup package list

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ version='0.1.0',
 description="",
 author="Tangi Roussel",
 packages=["pastis"],
-install_requires=['pygamma','numpy', 'suspect', 'pandas', 'pymapvbvd', 'xlrd', 'termcolor', 'nibabel', 'IPython', 'ismrmrd']
+install_requires=['pygamma','numpy', 'suspect', 'pandas', 'pymapvbvd', 'xlrd', 'termcolor', 'nibabel', 'IPython', 'brukerapi', 'ismrmrd']
 )
